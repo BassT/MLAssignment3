939b3c21d6877871696cf3672c24d49a6c76d1b8
Sebastian Richter <sebastian.richter@student.kit.edu>
2013-12-09 00:07:37 -0500

    updates and submission zip

--- exercise3.m
+++ exercise3.m
@@ -3,5 +3,6 @@ load('train.mat');
 coeff = pca(trainx, 'NumComponents', 5);
 for i = 1:5
     figure('name', strcat(num2str(i), 'th principal component'));
-    pcolor(reshape(coeff(:,i),19,19));axis([1 19 1 19]); title([num2str(i),'th principal component']); xlabel('pixels'); ylabel('pixels');
+    pcolor(reshape(flipud(coeff(:,i)),19,19));axis([1 19 1 19]); title([num2str(i),'th principal component']); xlabel('x pixels'); ylabel('y pixels');
+    colormap gray;
 end
--- exercise6.m
+++ exercise6.m
@@ -6,24 +6,28 @@ img = randi(size(training_face,1),1);
 % 1st principal component
 approx = coeff(:,1) .* training_face(img,:)';
 figure('name', 'approximation using first principal component');
-pcolor(reshape(approx,19,19));
-colorbar();title('approximation based upon the first principal component'); xlabel('pixel'); ylabel('pixel');
+pcolor(reshape(flipud(approx),19,19));
+colormap gray;
+title('first principal component'); xlabel('x pixel'); ylabel('y pixel');
 
 % first 2 principal components
 approx = (coeff(:,1) + coeff(:,2)) .* training_face(img,:)';
 figure('name', 'approximation using first two principal component');
-pcolor(reshape(approx,19,19));
-colorbar();title('approximation based upon the second principal component'); xlabel('pixel'); ylabel('pixel');
+pcolor(reshape(flipud(approx),19,19));
+colormap gray;
+title('first two principal components'); xlabel('x pixel'); ylabel('y pixel');
 
 % first 5 principal compnents
 approx = (coeff(:,1) + coeff(:,2) + coeff(:,3) + coeff(:,4) + ...
     coeff(:,5)) .* training_face(img,:)';
 figure('name', 'approximation using first 5 principal component');
-pcolor(reshape(approx,19,19));
-colorbar();title('approximation based upon the third principal component'); xlabel('pixel'); ylabel('pixel');
+pcolor(reshape(flipud(approx),19,19));
+colormap gray;
+title('first 5 principal components'); xlabel('x pixel'); ylabel('y pixel');
 
 % first 10 principal compnents
 approx = sum(coeff,2) .* training_face(img,:)';
 figure('name', 'approximation using first 10 principal component');
-pcolor(reshape(approx,19,19));
-colorbar();title('approximation based upon the fourtht principal component'); xlabel('pixel'); ylabel('pixel');
+pcolor(reshape(flipud(approx),19,19));
+colormap gray;
+title('first 10 principal components'); xlabel('x pixel'); ylabel('y pixel');
